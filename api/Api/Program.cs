39a6f3c1f2919b1effcd32d5eceae04730369c7a
Dobrev,Konstantin K.N. <529830@student.fontys.nl>
2026-03-04 13:23:24 +0100

    downloaded some packages

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -1,7 +1,6 @@
 using DAL;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,8 +11,6 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
 
 var app = builder.Build();
 
